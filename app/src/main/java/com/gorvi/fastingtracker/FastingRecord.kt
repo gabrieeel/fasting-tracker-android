0e4e619543aae602bb678bf72515a57d43aa9dc0
gabrieeel <gabrielp@gmail.com>
2024-01-23 09:23:30 -0300

    FastingRecord has a duration method. Increment minSdk and java version.

--- app/src/main/java/com/gorvi/fastingtracker/FastingRecord.kt
+++ app/src/main/java/com/gorvi/fastingtracker/FastingRecord.kt
@@ -2,6 +2,7 @@ package com.gorvi.fastingtracker
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.Duration
 import java.time.LocalDateTime
 
 @Entity(tableName = "fasting_records")
@@ -9,8 +10,13 @@ class FastingRecord(var startTime: LocalDateTime, var endTime: LocalDateTime?) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
 
+    fun duration(): Duration =
+        endTime?.let { Duration.between(startTime, it) } ?: Duration.ZERO
+
+
     override fun toString(): String {
         return "Start Time: ${this.startTime}\n" +
-                "End Time: ${this.endTime}"
+                "End Time: ${this.endTime}\n" +
+                "Duration: ${this.duration().toString().substring(2)}"
     }
 }
